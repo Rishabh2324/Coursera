85e646660f61a732cad6e98c89cc857bd3df89ac
Rishabh2324 <rishabh2401jain@gmail.com>
2019-09-19 11:47:18 +0530

    http

--- node-express/index.js
+++ node-express/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const hostname = 'localhost';
+const dishRouter = require('./routes/dishRouter');
 const port = '3000';
 const morgan = require('morgan');                      //for serving the html files
 const app = express();                                 //application is going to use express server
@@ -9,38 +10,10 @@ const app = express();                                 //application is going to
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json());
+app.use('/', dishRouter);
+app.use('/:dishId', dishRouter);
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    next();
-})
-app.get('/dishes', (req, res, next) => {
-    res.end('we will send you dish to you!');
-});
-app.post('/dishes/', (req, res, next) => {
-    res.end('Will update the dish :' + req.body.name + 'with details' + req.body.description);
 
-});
-app.put('/dishes/', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('Post operation not supported on /dishes/');
-})
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('we will send you dish : ' + req.params.dishId + 'to you!');
-});
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.end('Post operation not supported on /dishes/' + req.params.dishId);
-
-});
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish : ' + req.params.dishId + '\n');
-    res.end('Will update the dish :' + req.body.name + 'with details' + req.body.description);
-
-})
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish:' + req.params.dishId);
-})
 
 
 
